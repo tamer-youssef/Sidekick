01c98da7ef4ee21e4759575adaf8d737e33e3ec7
tamer <tameryoussef2@gmail.com>
2023-05-29 23:15:37 +0300

    Updated Cursor & squashed other bugs

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Windows.Kinect;
 using System;
 using System.Runtime.InteropServices;
-using System.Drawing;
 
 public class CursorController : MonoBehaviour
 {
@@ -31,8 +29,13 @@ public class CursorController : MonoBehaviour
         public MOUSEINPUT mi;
     }
 
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+    [DllImport("user32.dll")]
+    static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int x, int y);
+
+    private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+    private const uint MOUSEEVENTF_LEFTUP = 0x0004;
 
     public KinectSensor kinectSensor;
     public float cursorSpeed = 1000f;
@@ -40,8 +43,6 @@ public class CursorController : MonoBehaviour
 
     // Constants for mouse input type and flags
     private const int INPUT_MOUSE = 0;
-    private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-    private const int MOUSEEVENTF_LEFTUP = 0x04;
 
     private Body[] bodies;
     private BodyFrameReader bodyFrameReader = null;
@@ -65,7 +66,7 @@ public class CursorController : MonoBehaviour
             }
         }
     }
-
+    
     void Update()
     {
         if (this.bodyFrameReader != null)
@@ -89,12 +90,14 @@ public class CursorController : MonoBehaviour
                         var hand = body.Joints[JointType.HandRight];
                         var screenPos = Camera.main.WorldToScreenPoint(new Vector3(hand.Position.X * 100, hand.Position.Y * 100, hand.Position.Z * 10));
 
+
                         // Set the position of the cursor GameObject based on the tracked hand position
                         transform.position = Vector3.Lerp(transform.position, screenPos, Time.deltaTime * this.cursorSpeed);
 
                         // Check if the hand is in a closed fist
                         if (body.HandRightState == HandState.Closed)
                         {
+                            Debug.Log("hand is closed.");
                             if (!isHandClosed)
                             {
                                 // If the hand was previously open, mark the time when it was closed
@@ -112,6 +115,7 @@ public class CursorController : MonoBehaviour
                         }
                         else
                         {
+                            Debug.Log("hand is open");
                             // Reset the hand close state and time
                             isHandClosed = false;
                             handCloseTime = 0f;
@@ -137,35 +141,13 @@ public class CursorController : MonoBehaviour
 
     void PerformClickAction(Vector3 targetPosition)
     {
-        Debug.Log("left clicking!");
+        Debug.Log("Moving cursor and left clicking!");
         Debug.LogFormat("Performing click action at position ({0}, {1})", targetPosition.x, targetPosition.y);
 
-        // Set up an array of two INPUT structs representing a left mouse button down event and a left mouse button up event
-        INPUT[] inputs = new INPUT[2];
+        // Simulate a left mouse button down event
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 
-        // Set up the first struct with MOUSEINPUT data representing a left mouse button down event
-        inputs[0].type = INPUT_MOUSE;
-        inputs[0].mi.dx = (int)targetPosition.x;
-        inputs[0].mi.dy = Screen.height - (int)targetPosition.y;
-        inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-
-        // Set up the second struct with MOUSEINPUT data representing a left mouse button up event
-        inputs[1].type = INPUT_MOUSE;
-        inputs[1].mi.dx = (int)targetPosition.x;
-        inputs[1].mi.dy = Screen.height - (int)targetPosition.y;
-        inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
-
-        //Debug.Log("dx: " + inputs[0].mi.dx);
-        //Debug.Log("dy: " + inputs[0].mi.dy);
-        // Send the input events to the operating system
-        uint result = SendInput(2, inputs, Marshal.SizeOf(typeof(INPUT)));
-        if (result == 0)
-        {
-            Debug.Log("good");
-        } 
-        else if (result == 2)
-        {
-            Debug.Log("bad");
-        }
+        // Simulate a left mouse button up event
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
     }
 }
